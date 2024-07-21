cd7c73cedd2c381af21f24fec651743965b0009c
MingyuRomeoKim <rlaalsrb0466@naver.com>
2024-07-21 22:17:59 +0900

    create member service

---
+++ src/main/java/hello/hello_spring/services/MemberService.java
@@ -0,0 +1,41 @@
+package hello.hello_spring.services;
+
+import hello.hello_spring.domains.Member;
+import hello.hello_spring.repositories.MemberRepository;
+import hello.hello_spring.repositories.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 같은 이름 중복회원 가입 불가
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+        .ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+}
