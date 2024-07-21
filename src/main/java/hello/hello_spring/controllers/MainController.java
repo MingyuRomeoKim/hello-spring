bafdafe24bf0a3c4dad737d792ebfc689378966f
MingyuRomeoKim <rlaalsrb0466@naver.com>
2024-07-21 16:59:31 +0900

    test mvc

--- src/main/java/hello/hello_spring/controllers/MainController.java
+++ src/main/java/hello/hello_spring/controllers/MainController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -11,5 +12,12 @@ public class MainController {
     public String hello(Model model) {
         model.addAttribute("data","hello!!");
         return "Hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value="name",required = false) String name, Model model)
+    {
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
