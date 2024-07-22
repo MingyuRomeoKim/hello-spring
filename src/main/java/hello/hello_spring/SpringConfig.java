1bfb5df86261a2f4e056aa58e45b928484fe7496
MingyuRomeoKim <rlaalsrb0466@naver.com>
2024-07-22 17:12:27 +0900

    change connect jdbcTemplate to Jpa

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,25 +1,26 @@
 package hello.hello_spring;
 
-import hello.hello_spring.repositories.JdbcMemberRepository;
-import hello.hello_spring.repositories.JdbcTemplateMemberRepository;
+import hello.hello_spring.repositories.JpaMemberRepository;
 import hello.hello_spring.repositories.MemberRepository;
 import hello.hello_spring.services.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+    private EntityManager entityManager;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
-
 
     @Bean
     public MemberService memberService() {
@@ -30,6 +31,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(entityManager);
     }
 }
