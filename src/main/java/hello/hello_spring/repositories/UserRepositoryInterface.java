e1669ef4893b03731f2e2e412abad236003b51c2
MingyuRomeoKim <rlaalsrb0466@naver.com>
2024-07-30 23:13:41 +0900

    [issue-72206530] 레포지토리 코드 추가
    - add User Repository

--- src/main/java/hello/hello_spring/repositories/UserRepositoryInterface.java
+++ src/main/java/hello/hello_spring/repositories/UserRepositoryInterface.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepositoryInterface {
-    User save(User member);
+    User save(User user);
     Optional<User> findById(Long id);
     Optional<User> findByName(String name);
     Optional<User> findByEmail(String email);
