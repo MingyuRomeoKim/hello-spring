80065e589f3be01f46c6437dd5165b61dfde3881
MingyuRomeoKim <rlaalsrb0466@naver.com>
2024-08-01 18:11:18 +0900

    [issue-72206530] 회원,주문,할인 관련 기능 생성 - Only Use Java
    - 관심사 (AppConfig) 클래스 재정의

--- src/main/java/hello/hello_spring/java/AppConfig.java
+++ src/main/java/hello/hello_spring/java/AppConfig.java
@@ -1,19 +1,31 @@
 package hello.hello_spring.java;
 
+import hello.hello_spring.java.discount.DiscountPolicy;
 import hello.hello_spring.java.discount.FixDiscountPolicy;
+import hello.hello_spring.java.discount.RateDiscountPolicy;
 import hello.hello_spring.java.order.OrderService;
 import hello.hello_spring.java.order.OrderServiceImpl;
 import hello.hello_spring.java.user.MemoryUserRepository;
+import hello.hello_spring.java.user.UserRepository;
 import hello.hello_spring.java.user.UserService;
 import hello.hello_spring.java.user.UserServiceImpl;
 
 public class AppConfig {
 
     public UserService userService() {
-        return new UserServiceImpl(new MemoryUserRepository());
+        return new UserServiceImpl(memberRepository());
+    }
+
+    private UserRepository memberRepository() {
+        return new MemoryUserRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryUserRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
